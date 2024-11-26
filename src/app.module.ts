9f560a43016bf67cf6bd00f030588b8ca3c8ce30
AhmedYasser220 <ahmedfarrag1120@gmail.com>
2024-11-26 16:59:15 +0200

    Connection String Fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { QuizzesModule } from './quizzes/quizzes.module';
 import { ResponsesModule } from './responses/responses.module';
 import { ProgressModule } from './progress/progress.module';
 @Module({
-  imports: [UserModule,CourseModule,ModulesModule,QuizzesModule,ResponsesModule,ProgressModule,MongooseModule.forRoot('mongodb://Wightnight120:Qazxsw!!@e-learndb-shard-00-00.ksmzg.mongodb.net:27017,e-learndb-shard-00-01.ksmzg.mongodb.net:27017,e-learndb-shard-00-02.ksmzg.mongodb.net:27017/?ssl=true&replicaSet=atlas-pdfrlh-shard-0&authSource=admin&retryWrites=true&w=majority&appName=E-learnDB')],
+  imports: [UserModule,CourseModule,ModulesModule,QuizzesModule,ResponsesModule,ProgressModule,MongooseModule.forRoot('mongodb+srv://Wightnight120:Qazxsw!!@e-learndb.ksmzg.mongodb.net/')],
   controllers: [AppController],
   providers: [AppService],
 })
