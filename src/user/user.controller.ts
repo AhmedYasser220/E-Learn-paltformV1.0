16e01f8862522489fedddbdc59bcb81c0af8c508
batool612 <batool.ali612@yahoo.com>
2024-12-02 20:31:01 +0200

    second commit batool

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,5 +5,20 @@ import { user } from './Models/user.model';
 
 @Controller('users')
 export class UserController {
-  
+  constructor(private readonly userService: UserService) {}
+
+  // Fetch user profile by user_Id
+  @Get(':user_Id')
+  async getProfile(@Param('user_Id') user_Id: string) {
+    return this.userService.getUserProfile(user_Id);
+  }
+
+  // Update user profile by user_Id with partial data
+  @Put(':user_Id')
+  async updateProfile(
+    @Param('user_Id') user_Id: string,
+    @Body() updateData: Partial<user>
+  ) {
+    return this.userService.updateUserProfile(user_Id, updateData);
+  }
 }
