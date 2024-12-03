92865b64a454fc80d1f11ac9a919fb7602cb6a70
AhmedYasser220 <ahmedfarrag1120@gmail.com>
2024-12-03 11:52:33 +0200

    minor up

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,19 @@ import { ModulesModule } from './modules/modules.module';
 import { QuizzesModule } from './quizzes/quizzes.module';
 import { ResponsesModule } from './responses/responses.module';
 import { ProgressModule } from './progress/progress.module';
-import { BackupModule } from './backup/backup.module';
-
-
 
 @Module({
-  imports: [UserModule,CourseModule,ModulesModule,QuizzesModule,ResponsesModule,ProgressModule,BackupModule,MongooseModule.forRoot('mongodb+srv://Wightnight120:Qazxsw!!@e-learndb.ksmzg.mongodb.net/')],
+  imports: [
+    UserModule,
+    CourseModule,
+    ModulesModule,
+    QuizzesModule,
+    ResponsesModule,
+    ProgressModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://Wightnight120:Qazxsw!!@e-learndb.ksmzg.mongodb.net/',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
