0270cdf1e0cc726bcac165a3417f2e6cb61ec1d9
AhmedYasser220 <ahmedfarrag1120@gmail.com>
2024-12-20 11:40:21 +0200

    conflict resolved

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -1,24 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-<<<<<<< HEAD
 import { CourseService } from './course.service';
 import { CourseController } from './course.controller';
-import { CourseSchema } from './Model/course.model';
+import { Course, CourseSchema } from './Model/course.model';
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'Course', schema: CourseSchema }]), // Register schema here
+    MongooseModule.forFeature([{ name: Course.name, schema: CourseSchema }]), // Register schema here
   ],
-=======
-import { CourseController } from './course.controller';
-import { CourseService } from './course.service';
-import { course, CourseSchema } from './Model/course.nodel';
-
-@Module({
-  imports: [
-    MongooseModule.forFeature([{ name: course.name, schema: CourseSchema }]), // Register the schema
-  ],
-  controllers: [CourseController],
->>>>>>> origin/Abdullah
   providers: [CourseService],
 })
 export class CourseModule {}
